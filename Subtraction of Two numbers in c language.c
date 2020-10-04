79617604a628894b7054a2c687a07c0671c8e2c2
pc696852 <72335501+pc696852@users.noreply.github.com>
2020-10-04 16:48:29 +0530

    Create Subtraction of Two numbers in c language.c

---
+++ Subtraction of Two numbers in c language.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,minus=0;
+  printf("Enter any two numbers\n");
+  scanf("%d",&a);
+  scanf("%d",&b);
+  minus=a-b;
+  printf("SUM is=%d\n",minus);
+  return 0;
+}  
